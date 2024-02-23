1c7540d8aed8f0ad6d4e086445f0d7da3e39ed5e
lorenzodisanto <lore.disanto95@gmail.com>
2024-02-23 17:05:34 +0100

    feat: add form

--- result.php
+++ result.php
@@ -1,5 +1,11 @@
 <?php 
 
+// dichiarazione variabili
+$paragraph = $_GET['paragraph'];
+$badword = $_GET['badword'];
+
+// sostituzione badword con ***
+$new_paragraph = str_replace($badword, '***', $paragraph);
 ?>
 
 <!DOCTYPE html>
@@ -7,17 +13,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Result</title>
+    <title>PHP Badwords</title>
+
     <!-- Bootstrap -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-      crossorigin="anonymous"
-    />
+    <link 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+      rel="stylesheet" 
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+      crossorigin="anonymous">
+
     
 </head>
 <body>
-    
+    <div class="container mt-4">
+
+        <!-- stampa paragrafo -->
+        <div class="card mb-4 p-3"> 
+          <h4>Paragrafo</h4>
+          <p><?= $paragraph ?></p>
+          <p>Lunghezza paragrafo: <?= strlen($paragraph)?></p>
+        </div>
+       
+        <!-- stampa paragrafo con parola censurata -->
+        <div class="card p-3">
+          <h4>Nuovo paragrafo</h4>
+          <p><?= $new_paragraph ?></p>
+          <p>Lunghezza nuovo paragrafo: <?= strlen($new_paragraph)?></p>
+        </div>   
+
+    </div>
 </body>
 </html>
